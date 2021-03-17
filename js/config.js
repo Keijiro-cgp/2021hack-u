ea202d84ebd97b59a5dc9af769bf6e445eabdefc
Keijiro-cgp <keijiro.cgp@gmail.com>
2021-03-18 01:38:37 +0900

    試行錯誤1

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
 var config = {
   apiKey: "AIzaSyDgksUrBwXLDqdiYHJzMcEd1a8z-HyLvgc",
   authDomain: "hacku-e1737.firebaseapp.com",
-  databaseURL: "https://hacku-e1737.firebaseio.com",
   projectId: "hacku-e1737",
   storageBucket: "hacku-e1737.appspot.com",
   messagingSenderId: "233502997722",
