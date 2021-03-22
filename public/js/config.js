1d7187a97341809a9e82c583268334367f328931
Keijiro-cgp <keijiro.cgp@gmail.com>
2021-03-22 12:50:49 +0900

    ランキング機能凍結

--- public/js/config.js
+++ public/js/config.js
@@ -1,12 +1,12 @@
 var config = {
-  apiKey: "AIzaSyDgksUrBwXLDqdiYHJzMcEd1a8z-HyLvgc",
-  authDomain: "hacku-e1737.firebaseapp.com",
-  databaseURL: "https://hacku-e1737.firebaseio.com",
-  projectId: "hacku-e1737",
-  storageBucket: "hacku-e1737.appspot.com",
-  messagingSenderId: "233502997722",
-  appId: "1:233502997722:web:fb0764511bc75067bc39e3",
-  measurementId: "G-CRRD43Y9GP"
+  apiKey: "AIzaSyARplg_jb_az42E00-nd-5H2g8czn-m9_c",
+  authDomain: "hack-u2.firebaseapp.com",
+  databaseURL: "https://hack-u2.firebaseio.com",
+  projectId: "hack-u2",
+  storageBucket: "hack-u2.appspot.com",
+  messagingSenderId: "184105683079",
+  appId: "1:184105683079:web:4da313c47f5e801b6b9022",
+  measurementId: "G-2RV5KL752D"
 };
 if (!firebase.apps.length){
   firebase.initializeApp(config);
